9863261ab583233c0b8b114417a1e67c150c7585
Juan Tascon <juantascon@horlux.org>
2014-03-20 20:22:42 -0500

    better comments on http.py

--- http.py
+++ http.py
@@ -5,15 +5,21 @@ from flask import Flask, request, jsonify, json
 
 app = Flask(__name__, static_url_path='/static')
 
+# the database :)
 d = OrderedDict({})
 
+#
+# TODOS API: get / upsert / delete
+#
+
+# load all elements
 @app.route("/api/todos", methods=["GET"])
-def todos_get():
+def get_todos():
     return json.dumps(d.values())
-    
+
+# updates an element or insert it if it doesn't exists
 @app.route("/api/todos/<string:id>", methods=['PUT'])
 def upsert_todo(id):
-    print("upsert: "+id)
     if not d.get(id): d[id] = {}
     todo = d.get(id)
     
@@ -22,11 +28,13 @@ def upsert_todo(id):
     todo["done"] = request.json.get("done")
     
     return jsonify(result="ok")
-    
+
+# deletes an element
 @app.route("/api/todos/<string:id>", methods=['DELETE'])
 def delete_todo(id):
     del d[id]
     return jsonify(result="ok")
-    
+
+# run the http server
 if __name__ == '__main__':
     app.run(debug=True, port=8001)
