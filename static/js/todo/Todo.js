97a0df0a8fbd444b90a263392f6fdcb9f648fb02
Juan Tascon <juantascon@horlux.org>
2014-03-25 10:57:20 -0500

    xuuid is now random_uuid

--- static/js/todo/Todo.js
+++ static/js/todo/Todo.js
@@ -2,14 +2,14 @@ define([ "backbone" ], function(Backbone) {
   var Todo = Backbone.Model.extend({
     
     defaults: function() {
-      return { title: "edit me!", done: false, id: this.xuuid(), order: 0 };
+      return { title: "edit me!", done: false, id: this.random_uuid(), order: 0 };
     },
     
     toggle: function() {
       this.save({done: !this.get("done")});
     },
     
-    xuuid: function() {
+    random_uuid: function() {
       var _uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
         var r = Math.random()*16|0
         var v = c=='x' ? r : r&0x3|0x8;
